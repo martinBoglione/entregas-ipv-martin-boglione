62bef87ce43f291be59bdb65160e01fc6afffea1
Martin <martinboglione97@gmail.com>
2026-09-02 21:08:45 -0300

    Desafio 1

---
+++ entrega-1-martin-boglione/character_body_2d.gd
@@ -0,0 +1,11 @@
+extends CharacterBody2D
+
+@export var speed = 400
+
+func get_input():
+	var input_direction = Input.get_vector("Left", "Right", "Down", "Up")
+	velocity = input_direction * speed
+
+func _physics_process(delta):
+	get_input()
+	move_and_slide()
